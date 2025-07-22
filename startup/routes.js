3b2fd67544e436481a32c40196dd4f5649100ddc
Digvijay Singh <digupathania25@gmail.com>
2025-07-23 04:37:04 +0530

    removed admin

--- startup/routes.js
+++ startup/routes.js
@@ -2,7 +2,6 @@ const homeRoutes = require('../routes/home/home');
 const volunteerRoutes = require('../routes/volunteer/volunteer');
 const donateRoutes = require('../routes/donation/donate');
 const requestRoutes = require('../routes/request/request');
-const adminRoutes = require('../routes/admin/main');
 const userRoutes = require('../routes/user/user');
 const userProfileRoutes = require('../routes/user/profile');
 const subscriptionRoutes = require('../routes/user/subscription');
@@ -12,7 +11,6 @@ module.exports = (app) => {
     app.use('/', volunteerRoutes);
     app.use('/', donateRoutes);
     app.use('/', requestRoutes);
-    app.use('/admin', adminRoutes);
     app.use('/', userRoutes);
     app.use('/', userProfileRoutes);
     app.use('/', subscriptionRoutes);
